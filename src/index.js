1e6514a3e3042da605cf84d5b7cc6182a0ec9276
YesicaChui <yesicachuic@gmail.com>
2023-01-17 16:38:35 -0500

    reduciendo codigo

--- src/index.js
+++ src/index.js
@@ -3,69 +3,50 @@ function validate() {
   const valid = isValid(document.getElementById("cardnumber").value)
   if (valid) {
     divResult.innerHTML = `<p>La Tarjeta es Valida</p>`
-    //muestra correcto en div result// Show success in div#result
   }
   else {
     divResult.innerHTML = `<p>La Tarjeta es Invalida</p>`
-    //muestra mensaje de error en div result // Show error message in div#result
-  }
+ }
 }
-
-function sumaCifras(entrada) {//"12"
-  let res = 0;
-  for(elemento of entrada){//primera vuelta el elemento "1"
-    res=res+Number(elemento) 
-  } 
-  return res;
-}
-
 function isValid(creditCardNumber) {
-
-  let validador = true //variable validadora
-  if (creditCardNumber == "") {//es incorrecto si esta vacio
-    validador = false
-    return validador
-  } 
-
+  let validador = false //variable validadora
+  if (creditCardNumber == "") return validador //si esta vacio es incorrecto
   //algoritmo de Luhn
-
   //paso1 inversion
   const arregloInvertido = [...creditCardNumber].reverse()
-  console.log(arregloInvertido)
+  console.log(arregloInvertido) //arreglo con string
   let arregloInvertidoNumero = []
   for (elemento of arregloInvertido) {
     arregloInvertidoNumero.push(Number(elemento))
   }
   console.log(arregloInvertidoNumero)
-
-  //paso2 multiplicar por dos los que son de posicion par
+  //paso2 buscamos los de POSICION PAR y los multiplicamos x2
   for (let i = 0; i < arregloInvertidoNumero.length; i++) {
     if ((i + 1) % 2 == 0) {
       arregloInvertidoNumero[i] = arregloInvertidoNumero[i] * 2
-      //paso3 suma de digitos si son mayores que 10
-      if(arregloInvertidoNumero[i]>=10){
-         arregloInvertidoNumero[i] = sumaCifras(arregloInvertidoNumero[i].toString())
+      //paso3 suma de digitos si son mayores iguales que 10
+      if (arregloInvertidoNumero[i] >= 10) {//10,12,14,16,18
+        let numeroCadena = arregloInvertidoNumero[i].toString() //"16"
+        arregloInvertidoNumero[i] = Number(numeroCadena[0])+Number(numeroCadena[1])
       }
     }
   }
   console.log(arregloInvertidoNumero)
-  // paso5: sumar todas los elementos elementos del arreglo y verificar que sea multiplo de 10
+  // paso4: sumar todas los elementos del arreglo y verificar que sea multiplo de 10
   let acumulador = 0
-  for(element of arregloInvertidoNumero){
-    acumulador=acumulador+element
+  for (element of arregloInvertidoNumero) {
+    acumulador = acumulador + element
   }
   console.log(acumulador)
-  if(acumulador%10!=0){
-    validador = false
+  if (acumulador % 10 == 0) {
+    validador = true // es valido si es multiplo de 10
   }
-  //es creditcardnumber valido? // is creditCardNumber valid?
   return validador
 }
-
-const input = document.getElementById('cardnumber');
-//restringiendo que se acepte un numero de caracteres
-input.addEventListener('input', function () {
-  if (this.value.length > 16)
-    this.value = this.value.slice(0, 16);
-})
-
+//restringiendo como maximo 16 caracteres
+function maximaLongitud () {
+  const inputCard = document.getElementById('cardnumber');
+  if (inputCard.value.length > 16){
+    inputCard.value = inputCard.value.slice(0, 16);
+  }    
+}
